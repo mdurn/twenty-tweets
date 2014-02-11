a4905a8b11708aefd2d5992182f4b8fadaf6e9d0
Michael Durnhofer <mdurnhofer@gmail.com>
2014-02-10 20:51:08 -0800

    Integrate twitter omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,33 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :rememberable, :trackable
+         :rememberable, :trackable, :omniauthable,
+         :omniauth_providers => [:twitter]
+
+  ###############
+  # Validations #
+  ###############
 
   validates_presence_of :password, on: :create
   validates_length_of :password, within: Devise.password_length, allow_blank: false
+
+  #################
+  # Class Methods #
+  #################
+
+  class << self
+
+    def find_for_twitter_oauth(auth)
+      where(auth.slice(:provider, :uid)).first_or_create do |user|
+          user.provider = auth.provider
+          user.uid = auth.uid
+          user.password = Devise.friendly_token[0,20]
+          #user.name = auth.info.name   # assuming the user model has a name
+          #user.image = auth.info.image # assuming the user model has an image
+      end
+    end
+
+  end
+
 
 end
